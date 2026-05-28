cd277a5801cac8fd152efdddb9ca23b02c8ee02c
Odun <kolbasausage@gmail.com>
2026-05-28 13:25:49 +1200

    Fixed player animation, fixing the energy markers

--- energy_marker.gd
+++ energy_marker.gd
@@ -1,18 +1,12 @@
 extends Sprite2D
 
-# Energy cost where the marker should appear.
+@export var energy_bar: TextureProgressBar
 @export var energy_cost: float = 25.0
 
-# Maximum energy in your game.
-@export var max_energy: float = 100.0
-
-# Height of the energy bar in pixels.
-# Measure your bar image and type the height here.
-@export var bar_height: float = 400.0
-
 func _ready():
-	# Convert energy cost (0-100) into a percentage.
-	var ratio = energy_cost / max_energy
-	var y = bar_height * ratio
 
-	position.y = y
+	var ratio = energy_cost / energy_bar.max_value
+
+	var bar_height = energy_bar.size.y
+
+	position.y = energy_bar.position.y + (bar_height * (1.0 - ratio))
